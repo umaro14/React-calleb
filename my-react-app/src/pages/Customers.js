1e263dddefafca0137b6537f96372ef7c1bdb8c0
uamro14 <ajklaus14@gmail.com>
2024-11-05 17:44:07 +0000

    fixed my react app

--- my-react-app/src/pages/Customers.js
+++ my-react-app/src/pages/Customers.js
@@ -1,3 +1,29 @@
+
+import { useEffect, useState } from "react";
+
+
 export default function Customers() {
-    return <h1>Hello there!</h1>;
+
+    const [customers, setCustomers] = useState();
+
+    useEffect(() =>{
+        console.log("Fetching...")
+        fetch("http://localhost:8000/api/customers/")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+           setCustomers(data.customer);
+        })
+    }, []);
+
+
+    return (
+        <>
+            <h1>Here are our customers</h1>
+            {customers ? customers.map((customer) => (
+                <p key={customer.id}>{customer.name}</p>
+            )) : null}
+        </>
+    );
 }
+     
